545a75220a4ebdfee40c0e65048fd79198990a38
Axelhrdz <axelhernandez@Axel-Hernandez-L77X5JMQ7R.local>
2024-04-30 16:23:57 -0600

    images

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -20,12 +20,12 @@ const handleEdit = () => {
         <hr className='user__line' />
         <ul className='user__list'>
             <li className='user__item'><span>Email: </span><span>{user.email}</span></li>
-            <li className='user__item'><span>Birthday: </span><span><img src="public/gift-regular-24.png" alt="gift icon" width={'15px'} /> {user.birthday}</span></li>
+            <li className='user__item'><span>Birthday: </span><span><img src="../gift-regular-24.png" alt="gift icon" width={'15px'} /> {user.birthday}</span></li>
         </ul>
         <hr className='user__line' />
         <div className='user__buttons'>
-            <button className='user__btn btn__trash' onClick={handleDelete}><img src="public/trash-regular-24.png" alt="trash icon" width={'14px'} /></button>
-            <button className='user__btn btn__edit' onClick={handleEdit}><img src="public/pencil-solid-24.png" alt="pencil icon" width={'15px'} /></button>
+            <button className='user__btn btn__trash' onClick={handleDelete}><img src="../trash-regular-24.png" alt="trash icon" width={'14px'} /></button>
+            <button className='user__btn btn__edit' onClick={handleEdit}><img src="../pencil-solid-24.png" alt="pencil icon" width={'15px'} /></button>
         </div>
     </section>
   )
